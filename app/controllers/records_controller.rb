25d1f6c40e690d84705359989fe0460c0fca357d
German Aquila <g.axl.aquila@gmail.com>
2021-06-30 13:09:54 -0300

    Add records controller

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,2 +1,46 @@
 class RecordsController < ApplicationController
+    before_action :set_account
+    before_action :set_account_record, only: [:show, :update, :destroy]
+
+  # GET /accounts/:account_id/records
+  def index
+    json_response(@account.records)
+  end
+
+  # GET /accounts/:account_id/records/:id
+  def show
+    json_response(@record)
+  end
+
+  # POST /accounts/:account_id/records
+  def create
+    @account.records.create!(record_params)
+    json_response(@account, :created)
+  end
+
+  # PUT /accounts/:account_id/records/:id
+  def update
+    @record.update(record_params)
+    head :no_content
+  end
+
+  # DELETE /accounts/:account_id/records/:id
+  def destroy
+    @record.destroy
+    head :no_content
+  end
+
+  private
+
+  def record_params
+    params.permit(:flow_direction, :pending)
+  end
+
+  def set_account
+    @account = Account.find(params[:account_id])
+  end
+
+  def set_account_record
+    @record = @account.records.find_by!(id: params[:id]) if @account
+  end
 end
