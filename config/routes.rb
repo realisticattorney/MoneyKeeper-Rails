aab451706c4cdf855834a7b8d9ec7a49e0232d2c
German Aquila <g.axl.aquila@gmail.com>
2021-06-30 23:29:49 -0300

    Add Users controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
     resources :accounts do
     resources :records
   end
+    post 'auth/login', to: 'authentication#authenticate'
+    post 'signup', to: 'users#create'
 end
