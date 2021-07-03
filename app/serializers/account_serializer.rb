bb6b8e9b43ef8a2e1c38ab0f7ca1ebe22ccc8cae
German Aquila <g.axl.aquila@gmail.com>
2021-07-03 10:27:10 -0300

    Remove updated at from accounts serializer

--- app/serializers/account_serializer.rb
+++ app/serializers/account_serializer.rb
@@ -1,13 +1,5 @@
 class AccountSerializer < ActiveModel::Serializer
-  def attributes
-    hash = super
-    hash.each do |key, value|
-      if value.nil?
-        hash.delete(key)
-      end
-    end
-    hash
-  end
-  attributes :id, :created_by, :created_at, :updated_at
+   include NullAttributesRemover
+  attributes :id, :created_by, :created_at
   has_many :records
 end
