4216ae8735ee60b91827a0beb1561f37e1749f3f
German Aquila <g.axl.aquila@gmail.com>
2021-07-03 09:36:26 -0300

    Move module to another folder

--- app/serializers/account_serializer.rb
+++ app/serializers/account_serializer.rb
@@ -1,5 +1,13 @@
 class AccountSerializer < ActiveModel::Serializer
-  # include NullAttributesRemover
+  def attributes
+    hash = super
+    hash.each do |key, value|
+      if value.nil?
+        hash.delete(key)
+      end
+    end
+    hash
+  end
   attributes :id, :created_by, :created_at, :updated_at
   has_many :records
 end
