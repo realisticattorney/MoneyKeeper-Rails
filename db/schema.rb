8eec89202ca9a87e3ecf03ae49d359a90bd6d56d
German Aquila <g.axl.aquila@gmail.com>
2021-07-06 14:11:59 -0300

    Add Amount column to records table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_01_014257) do
+ActiveRecord::Schema.define(version: 2021_07_06_170958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2021_07_01_014257) do
     t.bigint "account_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "amount"
     t.index ["account_id"], name: "index_records_on_account_id"
   end
 
