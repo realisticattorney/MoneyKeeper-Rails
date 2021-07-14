fa3da770980423736602ab76f1312fd31b96451b
realisticattoney <g.axl.aquila@gmail.com>
2021-07-14 20:35:05 -0300

    Add email format validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :accounts, foreign_key: :created_by
+  validates :email, presence: true, format: { with: /\S+@\S+\.\S+/ }
   validates_presence_of :name, :email, :password_digest
 end
