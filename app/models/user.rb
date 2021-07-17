87058c9b1d10f2f5316d2eaca6cfb1613823eb4a
realisticattoney <g.axl.aquila@gmail.com>
2021-07-16 21:15:52 -0300

    reverse model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,5 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :accounts, foreign_key: :created_by
-  validates :email, presence: true, format: { with: /\S+@\S+\.\S+/ }
   validates_presence_of :name, :email, :password_digest
 end
